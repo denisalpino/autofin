1d30eb5cd660b89c443a196c26dc19144b40717b
denisalpino <denis.tomin.alpino@gmail.com>
2025-09-13 03:45:32 +0300

    style: align lines width and add some inline comments

--- src/preprocessing/feature_engineering.py
+++ src/preprocessing/feature_engineering.py
@@ -77,24 +77,47 @@ def calculate_indicator(
     open, high = df[base_columns["open"]], df[base_columns["high"]]
     low, close = df[base_columns["low"]], df[base_columns["close"]]
 
+    # Average True Range
     if name.startswith("ATR"):
-        atr = indicators.atr(high, low, close, length=config["window"], mamode=config["kind"])
+        atr = indicators.atr(
+            high,
+            low,
+            close,
+            length=config["window"],
+        mamode=config["kind"]
+        )
         return atr  # type: ignore
+
+    # Relative Strength Index
     elif name.startswith("RSI"):
         prices = df[config["price"]]
         rsi = indicators.rsi(prices, length=config["window"], scalar=1)
         return rsi  # type: ignore
+
+    # Bollinger Bands
     elif name.startswith("BBP"):
-        bb = indicators.bbands(close, length=config["window"], std=config["std"],
-                               mamode=config["kind"]).iloc[:, -2:]  # type: ignore
+        bb = indicators.bbands(
+            close,
+            length=config["window"],
+            std=config["std"],
+            mamode=config["kind"]
+        ).iloc[:, -2:]  # type: ignore
         bb.columns = ["BBB", "BBP"]
         return bb[config["output"]]  # type: ignore
+
+    # Moving Average Convergence Divergence (including signal and histogram)
     elif name.startswith("MACD"):
         prices = df[config["price"]]
-        macd = indicators.macd(prices, fast=config["short_window"],
-                               slow=config["long_window"], signal=config["signal_window"])  # type: ignore
+        macd = indicators.macd(
+            prices,
+            fast=config["short_window"],
+            slow=config["long_window"],
+            signal=config["signal_window"]
+        )  # type: ignore
         macd.columns = ["MACD", "MACD_Signal", "MACD_Hist"]  # type: ignore
         return macd[config["output"]]  # type: ignore
+
+    # Average Directional Index
     elif name.startswith("ADX"):
         adx = indicators.adx(
             high, low, close,
@@ -104,81 +127,147 @@ def calculate_indicator(
             scalar=1)
         adx.columns = ["ADX", "+DI", "-DI"]  # type: ignore
         return adx[config["output"]]  # type: ignore
+
+    # Donchain Channels
     elif name.startswith("DC"):
         dc = indicators.donchian(
-            high, low, lower_length=config["window"], upper_length=config["window"])
+            high,
+            low,
+            lower_length=config["window"],
+            upper_length=config["window"])
         dcr = pd.DataFrame()
+
         for col, ser in dc.items():  # type: ignore
             dcr[col] = ser
+
         dcr.columns = ["LowDC", "MidDC", "UpDC"]
         return dcr[config["output"]]
-    elif name.startswith("ERP"):  # Elder Ray Power
+
+    # Elder Ray Power
+    elif name.startswith("ERP"):
         ema = indicators.ema(close, length=config["window"])
         bears_power = high - ema
         bulls_power = low - ema
         bears_power.name = "BePo"
         bulls_power.name = "BuPo"
         return pd.concat([bears_power, bulls_power], axis=1)
+    # Mass Index (MI)
     elif name.startswith("MI"):
-        mi = indicators.massi(df.high, df.low, fast=config["short_window"], slow=config["long_window"])
+        mi = indicators.massi(
+            df.high,
+            df.low,
+            fast=config["short_window"],
+            slow=config["long_window"]
+        )
         return mi  # type: ignore
+    # Moving Average (including few kinds)
     elif name.startswith("MA_"):
         prices = df[config["price"]]
         ma = indicators.ma(config["kind"], prices, length=config["window"])
         return ma
+
+    # Volatility Ratio
     elif name == "volatility_ratio":
         vr = high / low - 1
         vr.name = "volatility_ratio"
         return vr
+
+    # Candle Strength
     elif name == "candle_strength":
         cs = ((close - open) / (high - low + 1e-5)).rename("candle_strength")
         return cs
+
+    # Absolute Body Differance
     elif name == "body":
         body = (open - close).abs().rename("body")
         return body
+
+    # Upper Wick (diff between high and open or close)
     elif name == "upper_wick":
         oc = pd.concat([open, close], axis=1)
         upper_wick = (high - oc.max(axis=1)).rename("upper_wick")
         return upper_wick
+
+    # Lower Wick (diff between low and open or close)
     elif name == "lower_wick":
         oc = pd.concat([open, close], axis=1)
         lower_wick = (oc.min(axis=1) - low).rename("lower_wick")
         return lower_wick
+
+    # Close-to-close differance (absolute return)
     elif name == "cc_diff":
         cc_diff = (close - close.shift(1)).rename("cc_diff")
         return cc_diff
+
+    # Ratio of Close - EMA and EMA
     elif name.startswith("price_to_ema"):
         ma = indicators.ma(config["kind"], close, length=config["window"])
         price_to_ema = ((close - ma) / ma).rename(f"price_to_ema_{config['window']}")
         return price_to_ema
+
+    # Z-Score close
     elif name.startswith("ZScore_"):
         ma_rolling = close.rolling(config["window"]).mean()
         std_rolling = close.rolling(config["window"]).std()
         zscore = ((close - ma_rolling) / std_rolling).rename(f"ZScore_{config['window']}")
         return zscore
+
+    # Keltner Channels
     elif name.startswith("KC"):
         kc = indicators.kc(high, low, close, length=config["window"], scalar=config["scalar"])
         kcr = pd.DataFrame()
+
         for col, ser in kc.items():  # type: ignore
             kcr[col] = ser
+
         kcr.columns = ["LowKC", "MidKC", "UpKC"]
         return kcr[config["output"]]
+
+    # Momentum with window
     elif name.startswith("MOM_"):
         mom = close.diff(config["window"]).rename(f"MOM_{config['window']}")
         return mom
+
+    # Skew of absolute returns per rolling window
     elif name.startswith("ret_skew_"):
-        ret_skew = (close - close.shift()).rolling(config["window"]).skew().rename(f"ret_skew_{config['window']}")
+        ret_skew = (
+            (close - close.shift())
+            .rolling(config["window"])
+            .skew()
+            .rename(f"ret_skew_{config['window']}")
+        )
         return ret_skew
+
+    # Kurtosis of absolute returns per rolling window
     elif name.startswith("ret_kurt_"):
-        ret_kurt = (close - close.shift()).rolling(config["window"]).kurt().rename(f"ret_kurt_{config['window']}")
+        ret_kurt = (
+            (close - close.shift())
+            .rolling(config["window"])
+            .kurt()
+            .rename(f"ret_kurt_{config['window']}")
+        )
         return ret_kurt
+
+    # Standart deviation of absolute returns per rolling window
     elif name.startswith("ret_std_"):
-        ret_std = (close - close.shift()).rolling(config["window"]).std().rename(f"ret_std_{config['window']}")
+        ret_std = (
+            (close - close.shift())
+            .rolling(config["window"])
+            .std()
+            .rename(f"ret_std_{config['window']}")
+        )
         return ret_std
+
+    # Mean of absolute returns per rolling window
     elif name.startswith("ret_mean_"):
-        ret_mean = (close - close.shift()).rolling(config["window"]).mean().rename(f"ret_mean_{config['window']}")
+        ret_mean = (
+            (close - close.shift())
+            .rolling(config["window"])
+            .mean()
+            .rename(f"ret_mean_{config['window']}")
+        )
         return ret_mean
-    raise ValueError(f"Unknown indicator {name} detected")
+    raise ValueError(f"Unknown indicator {name} detected!")
 
 
 def create_time_features(
@@ -198,27 +287,47 @@ def create_time_features(
     if minute:
         time_features = pd.concat([
             time_features,
-            encode_cyclic(timestamps.dt.minute.to_numpy(), col_name="minute", max_val=60)
+            encode_cyclic(
+                timestamps.dt.minute.to_numpy(),
+                col_name="minute",
+                max_val=60
+            )
         ], axis=1)
     if hour:
         time_features = pd.concat([
             time_features,
-            encode_cyclic(timestamps.dt.hour.to_numpy(), col_name="hour", max_val=24)
+            encode_cyclic(
+                timestamps.dt.hour.to_numpy(),
+                col_name="hour",
+                max_val=24
+            )
         ], axis=1)
     if day:
         time_features = pd.concat([
             time_features,
-            encode_cyclic(timestamps.dt.day.to_numpy(), col_name="day", max_val=timestamps.dt.days_in_month.to_numpy())
+            encode_cyclic(
+                timestamps.dt.day.to_numpy(),
+                col_name="day",
+                max_val=timestamps.dt.days_in_month.to_numpy()
+            )
         ], axis=1)
     if day_of_week:
         time_features = pd.concat([
             time_features,
-            encode_cyclic(timestamps.dt.day_of_week.to_numpy(), col_name="day_of_week", max_val=7)
+            encode_cyclic(
+                timestamps.dt.day_of_week.to_numpy(),
+                col_name="day_of_week",
+                max_val=7
+            )
         ], axis=1)
     if month:
         time_features = pd.concat([
             time_features,
-            encode_cyclic(timestamps.dt.month.to_numpy(), col_name="month", max_val=12)
+            encode_cyclic(
+                timestamps.dt.month.to_numpy(),
+                col_name="month",
+                max_val=12
+            )
         ], axis=1)
 
     return time_features
@@ -286,5 +395,6 @@ def create_price_target(
     price_col_name: str
 ) -> pd.Series:
     price = df[price_col_name]
-    target = ((price - price.shift(-1)) / price.shift(-1)).rename("target")
+    diff = price - price.shift(-1)
+    target = (diff / price.shift(-1)).rename("target")
     return target
